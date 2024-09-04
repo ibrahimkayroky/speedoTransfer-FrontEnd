cd6f8eff00352a9419f0de66e8a58bb3e4308e3a
Heba Darabeh <hebadarabeh02@gmail.com>
2024-09-04 10:53:01 +0300

    created a navbar component and applied custom styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from './shared_components/button/button.component';
 import { FooterComponent } from './shared_components/footer/footer.component';
+import { NavBarComponent } from './navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, ButtonComponent],
+  imports: [RouterOutlet, FooterComponent, ButtonComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/navbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavBarComponent {}
