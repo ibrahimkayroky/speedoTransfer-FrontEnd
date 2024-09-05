bfd33cf3ad96e8f291535651d9512a4a9448b37a
Yomna Ayman <yomnaa43@gmail.com>
2024-09-05 17:14:18 +0300

    Button Componenet Has Been Created

---
+++ src/app/shared_components/button/button.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+})
+export class ButtonComponent {
+  @Input() buttonType: 'primary' | 'secondary' = 'primary';
+  @Input() buttonText: string = 'Click  Me';
+}
