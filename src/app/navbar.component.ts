ffa12422293ed612f084d6ea74036a1d6015b40f
Heba Darabeh <hebadarabeh02@gmail.com>
2024-09-05 19:14:09 +0300

    Add button component to navbar component, and add routes for login and signup component pages

--- src/app/navbar.component.ts
+++ src/app/navbar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ButtonComponent } from './shared_components/button/button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ButtonComponent, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
