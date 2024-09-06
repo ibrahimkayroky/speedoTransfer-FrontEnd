198789f5ea2afb94c92efc006dd24467028acfa1
Heba Darabeh <hebadarabeh02@gmail.com>
2024-09-06 18:30:33 +0300

    Created a component for the mobile application section and added it to the home page

---
+++ src/app/shared_components/mobile-application/mobile-application.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-application',
+  standalone: true,
+  imports: [],
+  templateUrl: './mobile-application.component.html',
+  styleUrl: './mobile-application.component.scss'
+})
+export class MobileApplicationComponent {
+
+}
