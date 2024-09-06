44b280952b4cd63a53fbd35b4f0954b2c941b6be
Heba Darabeh <hebadarabeh02@gmail.com>
2024-09-06 17:58:32 +0300

    Update navbar links to change color when active

--- src/app/shared_components/navbar/navbar.component.ts
+++ src/app/shared_components/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterLink, ButtonComponent, CommonModule],
+  imports: [RouterLink, ButtonComponent, CommonModule, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
