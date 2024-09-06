753640cf2a328b3f43c3de6a151694a6f9315d56
YomnaaAyman <yomna227617@bue.edu.eg>
2024-09-07 01:35:36 +0300

    Create LoginPage and Insert components in

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { InputComponent } from '../../shared_components/input/input.component';
+import { ButtonComponent } from '../../shared_components/button/button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [InputComponent, ButtonComponent, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
