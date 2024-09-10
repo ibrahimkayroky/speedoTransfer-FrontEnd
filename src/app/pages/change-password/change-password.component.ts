bb2f954d32c31119792c6ca7d1d20ad6bf3059f0
Heba Darabeh <hebadarabeh02@gmail.com>
2024-09-10 21:20:59 +0300

    Create change password page

--- src/app/pages/change-password/change-password.component.ts
+++ src/app/pages/change-password/change-password.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { InputComponent } from '../../shared_components/input/input.component';
+import { ButtonComponent } from '../../shared_components/button/button.component';
 
 @Component({
   selector: 'app-change-password',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, InputComponent, ButtonComponent],
   templateUrl: './change-password.component.html',
   styleUrl: './change-password.component.scss',
 })
