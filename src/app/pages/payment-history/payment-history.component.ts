6456e12f7d679f1e2899a137ef61d4a3c7d7f74b
Heba Darabeh <hebadarabeh02@gmail.com>
2024-09-10 19:46:04 +0300

    Create payment history page

--- src/app/pages/payment-history/payment-history.component.ts
+++ src/app/pages/payment-history/payment-history.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { PaymentCardComponent } from '../../shared_components/payment-card/payment-card.component';
 
 @Component({
   selector: 'app-payment-history',
   standalone: true,
-  imports: [],
+  imports: [PaymentCardComponent],
   templateUrl: './payment-history.component.html',
-  styleUrl: './payment-history.component.scss'
+  styleUrl: './payment-history.component.scss',
 })
-export class PaymentHistoryComponent {
-
-}
+export class PaymentHistoryComponent {}
