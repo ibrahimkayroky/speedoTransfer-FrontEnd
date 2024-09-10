974778a0f63d8492a3a8718c3c6d8776b6e71b8d
Heba Darabeh <hebadarabeh02@gmail.com>
2024-09-10 20:47:26 +0300

    Create Settings page profile section

--- src/app/pages/change-password/change-password.component.ts
+++ src/app/pages/change-password/change-password.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-change-password',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './change-password.component.html',
   styleUrl: './change-password.component.scss',
 })
--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { InputComponent } from '../../shared_components/input/input.component';
+import { ButtonComponent } from '../../shared_components/button/button.component';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, InputComponent, ButtonComponent],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.scss'
+  styleUrl: './settings.component.scss',
 })
-export class SettingsComponent {
-
-}
+export class SettingsComponent {}
