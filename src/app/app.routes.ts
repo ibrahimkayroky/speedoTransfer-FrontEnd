e881b5a5d388aea70e779cd9c960ed213460f1de
Heba Darabeh <hebadarabeh02@gmail.com>
2024-09-10 17:52:03 +0300

    Edit the components routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { ErrorComponent } from './pages/error/error.component';
 import { PaymentHistoryComponent } from './pages/payment-history/payment-history.component';
 import { SettingsComponent } from './pages/settings/settings.component';
 import { ChangePasswordComponent } from './pages/change-password/change-password.component';
+import { MyProfileComponent } from './pages/my-profile/my-profile.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -17,6 +18,7 @@ export const routes: Routes = [
     path: 'my-account',
     component: MyAccountComponent,
     children: [
+      { path: 'my-profile', component: MyProfileComponent },
       { path: 'payment-history', component: PaymentHistoryComponent },
       { path: 'settings', component: SettingsComponent },
       { path: 'change-password', component: ChangePasswordComponent },
