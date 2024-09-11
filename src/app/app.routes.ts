0fef79127c51e3c8dd33255e0e927b07c17b232d
YomnaaAyman <yomna227617@bue.edu.eg>
2024-09-11 13:20:22 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,14 @@ import { HelpComponent } from './pages/help/help.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { ErrorComponent } from './pages/error/error.component';
-import { PaymentHistoryComponent } from './pages/payment-history/payment-history/payment-history.component';
-import { SettingsComponent } from './pages/settings/settings/settings.component';
-import { ChangePasswordComponent } from './pages/change-password/change-password/change-password.component';
-import { FavoriteComponent } from './pages/favorite/favorite.component';
+import { PaymentHistoryComponent } from './pages/payment-history/payment-history.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { ChangePasswordComponent } from './pages/change-password/change-password.component';
+import { MyProfileComponent } from './pages/my-profile/my-profile.component';
+import { AmountComponent } from './pages/amount/amount.component';
 import { ConfirmationComponent } from './pages/confirmation/confirmation.component';
 import { PaymentComponent } from './pages/payment/payment.component';
-import { AmountComponent } from './pages/amount/amount.component';
+import { FavoriteComponent } from './pages/favorite/favorite.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -26,12 +27,18 @@ export const routes: Routes = [
       { path: 'favorite', component: FavoriteComponent },
     ],
   },
-  { path: 'my-account', component: MyAccountComponent },
+  {
+    path: 'my-account',
+    component: MyAccountComponent,
+    children: [
+      { path: 'my-profile', component: MyProfileComponent },
+      { path: 'payment-history', component: PaymentHistoryComponent },
+      { path: 'settings', component: SettingsComponent },
+      { path: 'change-password', component: ChangePasswordComponent },
+    ],
+  },
   { path: 'help', component: HelpComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'payment-history', component: PaymentHistoryComponent },
-  { path: 'settings', component: SettingsComponent },
-  { path: 'change-password', component: ChangePasswordComponent },
   { path: '**', component: ErrorComponent },
 ];
