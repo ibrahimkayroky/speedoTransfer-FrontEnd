9202027b3ae942994f980992f32ac1a635775cfa
Heba Darabeh <hebadarabeh02@gmail.com>
2024-09-12 01:27:56 +0300

    update API call for user registration

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { InputComponent } from '../../shared_components/input/input.component';
 import { ButtonComponent } from '../../shared_components/button/button.component';
 import { CommonModule } from '@angular/common';
@@ -6,12 +7,11 @@ import { FormsModule } from '@angular/forms';
 import { LoginComponent } from '../login/login.component';
 import { HomeComponent } from '../home/home.component';
 import { RouterLink } from '@angular/router';
-import { AuthService } from '../../services/services/auth.service';
+import { AuthService, user } from '../../services/services/auth.service';
 import * as constants from './constants';
 import {
   FormBuilder,
   FormGroup,
-  FormControl,
   Validators,
   ReactiveFormsModule,
 } from '@angular/forms';
@@ -40,10 +40,14 @@ export function passwordMatchValidator(
     HomeComponent,
     RouterLink,
     ReactiveFormsModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss',
 })
+export class SignupComponent implements OnInit {
+  loginForm: FormGroup;
+  formSubmitted = false;
 export class SignupComponent implements OnInit {
   loginForm: FormGroup;
   formSubmitted = false;
@@ -71,16 +75,63 @@ export class SignupComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  constructor(private authService: AuthService, private fb: FormBuilder) {
+    this.loginForm = this.fb.group(
+      {
+        name: ['', [Validators.required]],
+        email: ['', [Validators.required, Validators.email]],
+        country: ['', [Validators.required]],
+        dayOfBirth: ['', [Validators.required]],
+        monthOfBirth: ['', [Validators.required]],
+        yearOfBirth: ['', [Validators.required]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        confirmpassword: ['', [Validators.required, Validators.minLength(8)]],
+      },
+      {
+        validators: passwordMatchValidator, // Apply the custom validator
+      }
+    );
+  }
+
+  ngOnInit(): void {}
+
   signUp() {
-    this.authService.register().subscribe((response) => {
-      console.log(response);
-    });
+    if (this.loginForm.valid) {
+      const formData = this.loginForm.value;
+
+      const birthDate = new Date(
+        `${formData.yearOfBirth}-${formData.monthOfBirth}-${formData.dayOfBirth}`
+      ).toISOString();
+
+      const requestBody: user = {
+        name: formData.name,
+        email: formData.email,
+        password: formData.password,
+        country: formData.country,
+        birthDate: birthDate, // Pass the formatted birthDate
+      };
+
+      this.authService.register(requestBody).subscribe({
+        next: (response) => {
+          console.log('Registration successful:', response);
+        },
+        error: (err) => {
+          console.error('Registration failed:', err);
+        },
+        complete: () => {
+          console.log('Registration process completed');
+        },
+      });
+    } else {
+      console.log('Form is invalid');
+    }
   }
 
   onSubmit() {
     console.log(this.loginForm);
     this.formSubmitted = true;
     if (this.loginForm.valid) {
+      this.signUp();
       console.log('Form Submitted Successfully:', this.loginForm.value);
     } else {
       console.log('Form not valid');
